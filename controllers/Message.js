62022682de945279d2062e185ec4c1e7fc08e11e
Feranmi Adeniji <devferanmi@gmail.com>
2023-09-24 22:38:21 +0100

    support more than 16 character messages

--- controllers/Message.js
+++ controllers/Message.js
@@ -19,9 +19,9 @@ const AddMessageToQueue = WrapHandler(async (req, res) => {
 //   if (body.message.length > 16) {
 //     return res.status(400).send({ message: "message longer than 16" });
 //   }
-//   if (body.name.length > 16) {
-//     return res.status(400).send({ message: "message longer than 16" });
-//   }
+  if (body.name.length > 16) {
+    return res.status(400).send({ message: "message longer than 16" });
+  }
   body.messageID = generateID();
   // save to db
   const create = await MessageModel.create(body);
